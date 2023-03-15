d45d52bfe48a78dc044f3bb692b83cbf3bba2f25
Etienne <emoureton@gmail.com>
2023-03-15 12:19:01 +0100

    update readme

--- front/app.vue
+++ front/app.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
     <h1>Init</h1>
-    {{ username }}
-    {{ roomID }}
     <RoomManager />
   </div>
 </template>
 
 <script setup lang="ts">
-const { roomID, username } = useStore()
 </script>
